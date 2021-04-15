120d37b75540da3c03b0fe6b8da9a94446e0f19e
jancaitis <79664863+jancaitis@users.noreply.github.com>
2021-04-15 13:08:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace jauns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b =0;
+            
+            for( a =1; a < 10; a++)
+            {
+                for ( b = 0; b < 10; b++)
+                {   
+                    int sum = a +b;
+                 Console.WriteLine($"{a}{b}, {a}+{b} = {sum}");
+                    
+                }
+            }
+            
+
+
+
+          
+        }
+    }
+}
